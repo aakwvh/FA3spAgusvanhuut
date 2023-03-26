7b5cb59f44a9c19167fb0bfe65b0c4170b209dd4
Agus <amsterdam66>
2023-03-26 23:43:55 +0200

    in dit commit  wordt er een fucnite gemaakt die in de database gezocht naar de product id die dezelfde category en sub category hebben als aangegeven product id. het return een product id die vergelijk baar is als aangegeven product

--- main.py
+++ main.py
@@ -21,19 +21,41 @@ dan selecteer je een product id die dezelfde category en sub_category hebben en 
 """
 
 def geef_brand_category(p_id):
+    """
+    deze functie zorg ervoor, wanneer je bepaalde product_id geeft dat je het soort  brand,category en sub_category terugkrijgt.
+    """
     cur.execute(f"""SELECT product_id,brand, name,category,sub_category
 FROM product
 WHERE product_id = '{p_id}';
 """
 )
     rows = cur.fetchall()
-    content_filtering = list()
+    criteria = list()
 
     for item in rows:
-        content_filtering.append(item[1])
-        content_filtering.append(item[3])
-        content_filtering.append(item[4])
-    return(content_filtering)
+        criteria.append(item[3])
+        criteria.append(item[4])
+    return(criteria)
 
 print(geef_brand_category("44130"))
+
+
+def geef_vergelijkbare_product(criteria):
+    """"
+    in deze functie wordt er in de database gezocht naar de product id die dezelfde category en
+    sub category hebben als aangegeven product id.
+
+    """
+    cur.execute(f"""SELECT product_id,brand, name,category,sub_category
+    FROM product
+    WHERE category = '{criteria[0]}' and sub_category = '{criteria[1]}' ;
+    """
+                )
+    rows = cur.fetchall()
+    meest_vergelijkbare_product = list()
+
+    for item in rows:
+        meest_vergelijkbare_product.append(item[0])
+    return(meest_vergelijkbare_product)
+
 
